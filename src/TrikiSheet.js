6332de8644a8025520b42ea7bf17aff551905b1c
Oier Bravo Urtasun <oierbravo@gmail.com>
2018-05-01 04:48:09 +0200

    more changes

--- src/TrikiSheet.js
+++ src/TrikiSheet.js
@@ -2,21 +2,26 @@ import React from 'react';
 import SheetInfo from './SheetInfo';
 import SheetNotes from './SheetNotes';
 
-class Sheet extends React.Component {
-  render() {
-    const song = {
-
-      info: {
-        title:"Gure super abestia"
-      },
-      notes: [{zenbakia:23,norabidea:'ireki',type:'nota'},{zenbakia:21,norabidea:'ireki',type:'nota'},{zenbakia:19,norabidea:'ireki',type:'nota'},{type:'silence'},{zenbakia:22,norabidea:'itxi',type:'nota'},{type:'silence'},{zenbakia:23,norabidea:'ireki',type:'nota'},{type:'silence'},{zenbakia:22,norabidea:'itxi',type:'nota'}]
+class TrikiSheet extends React.Component {
+  constructor(props) {
+    super(props);
+    if(typeof props.sheet === 'undefined'){
+      this.sheet = {
+        info: {
+          title:"Gure super abestia"
+        },
+        notes: [{zenbakia:23,norabidea:'ireki',type:'nota'},{zenbakia:21,norabidea:'ireki',type:'nota'},{zenbakia:19,norabidea:'ireki',type:'nota'},{type:'silence'},{zenbakia:22,norabidea:'itxi',type:'nota'},{type:'silence'},{zenbakia:23,norabidea:'ireki',type:'nota'},{type:'silence'},{zenbakia:22,norabidea:'itxi',type:'nota'}]
+      }
     }
+  }
+
+  render() {
     return (
       <div className="sheet">
-        <SheetInfo info={song.info}></SheetInfo>
-        <SheetNotes notes={song.notes}></SheetNotes>
+        <SheetInfo info={this.sheet.info}></SheetInfo>
+        <SheetNotes notes={this.sheet.notes}></SheetNotes>
       </div>
     );
   }
 }
-export default Sheet
+export default TrikiSheet
